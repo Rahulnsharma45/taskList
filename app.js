18432b9af7805b5ff291d7525a9479d92ef86445
Rahulnsharma45 <djrahul8130565174@gmail.com>
2020-01-08 01:20:10 +0530

    Delete filter tasks

--- app.js
+++ app.js
@@ -15,6 +15,12 @@ loadEventListener();
 function loadEventListener() {
     //Add task event
     form.addEventListener('submit', addTask);
+    // Remove task event
+    taskList.addEventListener('click', removeTask);
+    // Clear task event
+    clearBtn.addEventListener('click', clearTasks);
+    // Filter tasks event
+    filter.addEventListener('keyup', filterTasks);
 }
 
 //Add Task
@@ -49,4 +55,38 @@ function addTask(e) {
 
   console.log(li);
   e.preventDefault();
+}
+
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item')) {
+      if(confirm('Are you Sure?')) {
+       e.target.parentElement.parentElement.remove();
+      }
+    }
+}
+
+// Clear Tasks
+function clearTasks() {
+  //taskList.innerHTML = '';
+
+  // Faster
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+// Filter tasks
+
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach
+  (function(task) {
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
